12952453fabc38bcede32b5a82dd380b2aba4744
Gaurav Singla <gauravsingla482@gmail.com>
2023-04-23 15:49:20 +0530

    Push to Github

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useSearchParams,
+} from "react-router-dom";
+import { StrictMode } from "react";
+import ThemeContext from "./ThemeContext";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/"> Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
